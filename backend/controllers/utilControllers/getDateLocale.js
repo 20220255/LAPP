f70c1840e0515e33d8879a62048e37d8675514db
20220255 <118017744+20220255@users.noreply.github.com>
2024-09-05 06:22:52 +1200

    1. Removed console.logs

--- backend/controllers/utilControllers/getDateLocale.js
+++ backend/controllers/utilControllers/getDateLocale.js
@@ -1,11 +1,7 @@
 
 const getLocaleDate = (date) => {
-
     const fullDate = date.split('/')
     return formatedDate = fullDate[2] + '-' + fullDate[0] + '-' + fullDate[1]
-    // console.log('date - ', date)
-    // const month = date.getMonth() + 1
-    // return date.getFullYear() + '-' + month + '-' + date.getDate()
 }
 
 module.exports = { getLocaleDate };
