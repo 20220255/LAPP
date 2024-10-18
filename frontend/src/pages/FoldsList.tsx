2e425497fecb7648b976cca532962ba5822a4477
20220255 <118017744+20220255@users.noreply.github.com>
2024-10-18 10:47:42 +0800

    removed comments

--- frontend/src/pages/FoldsList.tsx
+++ frontend/src/pages/FoldsList.tsx
@@ -43,46 +43,7 @@ const FoldsList = () => {
     /** Gets all the sales list from sales state */
     const { isLoading, salesList, isSuccess } = useSelector((state: RootState) => state.sales)
 
-    // type ValuePiece = Date | null | string;
-    // type Value = ValuePiece | [ValuePiece, ValuePiece];
-
-    //* new Date() - currrent
-    // const [dateValue, onChange] = useState<Value>([new Date(), new Date()]);
-
     const dispatch = useDispatch<AppDispatch>()
-
-    // useEffect(() => {
-    //     /** This will trigger if the dates for the date picker has changed - dateValue */
-    //     /* Get the start date for filtering the sales list for the data grid */
-    //     const startDate = dateValue?.toString().split(',')[0]
-    //     const endDate = dateValue?.toString().split(',')[1]
-
-    //     if (endDate && startDate) {
-    //         /** Get the start date from the date picker and subtract 1. This will correctly display the sales on the data grid */
-    //         const minus1StartDate = new Date(startDate)
-    //         minus1StartDate.setDate(minus1StartDate.getDate() - 1)
-
-    //         /** Get the end date from the date picker */
-    //         const plus1EndDate = new Date(endDate);
-    //         plus1EndDate.setDate(plus1EndDate.getDate());
-
-    //         /** Due to typescript, needed to validate start and end date first */
-    //         if (minus1StartDate && plus1EndDate) {
-    //             const dayjsStartDate = dayjs(minus1StartDate).format('YYYY-MM-DD')
-    //             const dayjsEndDate = dayjs(plus1EndDate).format('YYYY-MM-DD')
-
-    //             /** If user is admin, all sales will be displayed otherwise, Only the sales entered by the user will be displayed hence the isAdmin parameter*/
-    //             const getUser = async () => {
-    //                 await getMyFoldsList(user._id, dayjsStartDate, dayjsEndDate, user.isAdmin)
-    //             }
-    //             getUser()
-    //         }
-    //     }
-
-    //     // eslint-disable-next-line react-hooks/exhaustive-deps
-    // }, [dateValue, userValue, salesList])
-
-
 
     /** Get the Total Folds computation function */
     const getTotalFolds = (salesList: SalesListType[]): number => {
