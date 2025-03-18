72bc6204cb4158b61df0dee4b33e8c77b0740b4c
20220255 <118017744+20220255@users.noreply.github.com>
2025-03-18 18:54:18 +0800

    Changed bcrypt.compare to bcrypt.compareSync

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -52,8 +52,7 @@ const registerUser = asyncHandler(async (req, res) => {
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
-
-  if (user && bcrypt.compare(password, user.password)) {
+  if (user && bcrypt.compareSync(password, user.password)) {
     res.status(200).json({
       _id: user._id,
       firstName: user.firstName,
